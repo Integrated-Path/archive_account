8a68c7ca0f710c406c2f6bc36ddeff1e832dc437
yousifamer <yousif.amer@int-path.com>
2023-08-01 10:35:19 +0300

    UPDATE v16

--- __manifest__.py
+++ __manifest__.py
@@ -1,23 +1,20 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Archive Accounts",
-
     'summary': """This module lets you archive account""",
-
     'author': "Integrated Path",
     'website': "https://www.int-path.com",
-
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Accoounting',
-    'version': '13.0',
-
+    'version': '1.0',
+     'license': 'LGPL-3',
     # any module necessary for this one to work correctly
     'depends': ['base', 'account'],
-
     # always loaded
     'data': [
         'views/views.xml',
     ],
+    'license': 'LGPL-3',
 }
--- models/models.py
+++ models/models.py
@@ -1,6 +1,7 @@
 from odoo import models, fields, api
 
-class AccountAccount(models.Model):
-	_inherit = "account.account"
 
-	active = fields.Boolean("Active", default=True, copy=False, readonly=True)
+class AccountAccount(models.Model):
+    _inherit = "account.account"
+
+    active = fields.Boolean("Active", default=True, copy=False)
